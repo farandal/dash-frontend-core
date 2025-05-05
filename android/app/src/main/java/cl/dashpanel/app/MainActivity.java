494dc1d91fb0281e267c08badb8110d9fb5dc297
Francisco Aranda <farandal@gmail.com>
2025-05-04 22:29:10 -0400

    chore: commented firebase in mainactivity

--- android/app/src/main/java/cl/dashpanel/app/MainActivity.java
+++ android/app/src/main/java/cl/dashpanel/app/MainActivity.java
@@ -15,11 +15,11 @@ public class MainActivity extends BridgeActivity {
         // Initialize Pusher Push Notifications
         try {
             Log.d(TAG, "Initializing Pusher Push Notifications");
-            PushNotifications.start(getApplicationContext(), "8e70908f-1020-47ed-b3bd-57d4c177f1f1");
+            //PushNotifications.start(getApplicationContext(), "8e70908f-1020-47ed-b3bd-57d4c177f1f1");
             Log.d(TAG, "Pusher Push Notifications initialized successfully");
             
             // Subscribe to the "hello" interest
-            PushNotifications.addDeviceInterest("hello");
+            //PushNotifications.addDeviceInterest("hello");
             Log.d(TAG, "Subscribed to 'hello' interest");
             
             // You can log the device ID if needed
