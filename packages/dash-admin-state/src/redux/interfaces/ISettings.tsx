011a864d4f7f66875066c128a8cbea77ae8725a6
Francisco Aranda <farandal@gmail.com>
2025-05-25 03:07:18 -0400

    chore: App settings state for i18n

--- packages/dash-admin-state/src/redux/interfaces/ISettings.tsx
+++ packages/dash-admin-state/src/redux/interfaces/ISettings.tsx
@@ -1,5 +1,11 @@
 import { IDASHResourceGroupsIcons } from "./IDashResourceGroupsIcons";
 
+export interface ILocale {
+    languageId: string;
+		locale: string;
+		name: string;
+		icon: string;
+}
 // import IDashAutoAdminResourceConfig from "@dash-auto-admin/interfaces/IDashAutoAdminResourceConfig";
 export interface ISettingsState {
 	loading: boolean;
@@ -11,12 +17,8 @@ export interface ISettingsState {
   groupIcons?: IDASHResourceGroupsIcons;
 	isDirectionRTL: boolean;
 	// resources: IDashAutoAdminResourceConfig[]
-	locale: {
-		languageId: string;
-		locale: string;
-		name: string;
-		icon: string;
-	};
+	locale: string;
+  availableLocales: ILocale[];
 	sidebarExpandedWidth?: number;
 	sidebarCollapsedWidth?: number;
 }
