1bda8169e6ce69f5e112e996523dd4edb04fff4e
Francisco Aranda <farandal@gmail.com>
2025-07-10 14:27:15 -0400

    feat: added non hook version of axios hook

--- packages/dash-axios-hook/src/index.tsx
+++ packages/dash-axios-hook/src/index.tsx
@@ -1,4 +1,4 @@
-export { default as useAxios } from "./hooks/useAxios";
+export { default as useAxios, createAxiosInstance } from "./hooks/useAxios";
 export { default as useAxiosGetWithStore } from "./hooks/useAxiosWithStore";
 export type { IDashAutoAdminBackendError,IDashAutoAdminDefaultBackendStructure } from "./interfaces/IDashAutoAdminBackendError";
 export { default as processAxiosError } from "./hooks/processAxiosErrorFunction";
