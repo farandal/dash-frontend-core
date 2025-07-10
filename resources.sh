1680b3f35eb32164d73301cb0742b47144e1f279
Francisco Aranda <farandal@gmail.com>
2025-07-10 14:30:18 -0400

    feat: helper script to generate assets for android ios

--- resources.sh
+++ resources.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx @capacitor/assets generate --assetPath=./apps/dash/resources --iconBackgroundColor=#FFFFFF --iconBackgroundColorDark=#FFFFFF --splashBackgroundColor=#FFFFFF --splashBackgroundColorDark=#FFFFFF --android
+npx @capacitor/assets generate --assetPath=./apps/dash/resources --iconBackgroundColor=#E8E8E8 --iconBackgroundColorDark=#191919 --splashBackgroundColor=#E8E8E8 --splashBackgroundColorDark=#191919 --android
 npx cap sync
